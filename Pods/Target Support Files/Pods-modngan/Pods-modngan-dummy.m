ba2c185ece0572c41390564a87a25a81f5a3911a
CHAKRIT PANIAM <Chakrit69@gmail.com>
2017-11-16 20:56:57 +0700

    ignore pod

--- Pods/Target Support Files/Pods-modngan/Pods-modngan-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_modngan : NSObject
-@end
-@implementation PodsDummy_Pods_modngan
-@end
--- Pods/Target Support Files/Pods-modnganTests/Pods-modnganTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_modnganTests : NSObject
-@end
-@implementation PodsDummy_Pods_modnganTests
-@end
